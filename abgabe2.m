69db9054702d89905629f745859bcf6de96964c9
marpme <cryzar95@googlemail.com>
2017-07-07 15:52:43 +0200

    Saving files before refreshing line endings

--- abgabe2.m
+++
@@ -1,1 +0,0 @@
-x = 0; function y = a (x)   y = x.*expm1(x);endfunctionfunction y = b (x)   y = sin(x) + 2.*x.^(0.5);endfunction%interval = [9,9]%[ x ] = fzero(@a,2);t = -100:0.1:100;plotyy(@a, @b, [-10, 10, -10, 10]) grid;axis ("auto");
