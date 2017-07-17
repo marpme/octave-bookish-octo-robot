bd097c077098303e5964f37c4a1905a08b58c9f2
marpme <cryzar95@googlemail.com>
2017-07-17 09:39:11 +0200

    renames abgabe7 to 8 and adds the real abgabe7! Let's try it.

--- abgabe7.m
+++ abgabe7.m
@@ -2,107 +2,71 @@ clear;
 clc;
 more off;
 
+P = [];
 
-function [a, b] = linearReg(x,y)
-  n = length(x);
-  xquer = sum((1/n).*x);
-  yquer = sum((1/n).*y);
-  Sxy = (1/(n-1)) .* (sum(x.*y) - n.*xquer.*yquer);
-  Sx2 = (1/(n-1)) .* (sum((x.-xquer).^2));
-  a = Sxy./Sx2;
-  b = yquer .- a .* xquer;
+##
+## Ich gehe von einer Sortierten 
+## Reihenfolge von Zeichen aus!
+##
+function [ bitList ] = fanoEncoding(S, P)
+  DMAX = length(P);
+  ## loop durch jedes Zeichen (jedes muss encoded werden!)
+  cw_list = {};
+
+  for itr = 1:DMAX
+    if (P(itr) != 0)
+      digits = ceil(-log2(P(itr)));
+    else
+      digits = 0;
+    endif
+
+    Psum = sum ([0 P](1:itr));
+    s = [];
+    for i = 1:digits;
+      Psum = 2 * Psum;
+      if (Psum >= 1)
+        s = [s 1];
+        Psum = Psum - 1;
+      else
+        s = [s 0];
+      endif
+    endfor
+    cw_list{itr} = s;
+  endfor 
+  
+  bitList = cw_list;
 endfunction
 
-function [a,b,c] = quadraticReg(x,y)
-  n = length(x);
-  
-  A = [
-    sum(x .^ 4), sum(x .^ 3), sum(x .^ 2);
-    sum(x .^ 3), sum(x .^ 2), sum(x);
-    sum(x .^ 2), sum(x),      n;
-  ];
-  
-  B = [
-    sum((x .^ 2) .* y);
-    sum(x .* y);
-    sum(y);
-  ];
-  
-  X = A \ B;
-  a = X(1);
-  b = X(2);
-  c = X(3);
-  
-endfunction
+## aufgabe 1 ##
 
-function [a, b] = expReg(x,y)
-  u = x;
-  v = log(y);
-  
-  [c, d] = linearReg(u, v);
-  
-  a = exp(d);
-  b = c;
-endfunction
+zeichen = [ "A", "B", "C", "D", "E", "F" ];
+pZufall = [ 0.5, 0.25, 0.10, 0.08, 0.05, 0.02 ];
 
-### Aufgabe 1 ###
+bitList = fanoEncoding(zeichen, pZufall);
+disp("Encoding fuer Aufgabe 1) "),disp(bitList);
+EvonN = 0;
+Eideal = 0;
+
+for i = [1:length(pZufall)] 
+  EvonN += length(bitList{1,i}) * pZufall(i);
+  Eideal -= pZufall(i)*log2(pZufall(i));
+endfor
+disp("Zusatz Aufgabe 1)");
+disp(Eideal/EvonN);
+
+## aufgabe 2 ##
 clear;
 
-xOne = [ 10.6 14.0 18.1, 23.2, 25.0, 26.4, 30.5, 32.5, 36.6, 40.1 ];
-yOne = [ 8.6, 11.5, 12.4, 15.6, 15.1, 17.7, 18.9, 18.6, 21.3, 24.3 ];
-[a, b] = linearReg(xOne,yOne);
-f = @(x) a.*x.+b;
-xerg = [0:0.1:50];
-subplot(2,3,1);
-plot(xOne,yOne, "*", xerg, f(xerg));
-printf("1) Funktion: %d * x + %d \n", a, b);
+zeichen = [ "x1", "x2", "x3", "x4", "x5", "x6"];
+pZufall = [0.30 0.28 0.12 0.12 0.10 0.08];
+bitList = fanoEncoding(zeichen, pZufall);
+EvonN = 0;
+Eideal = 0;
 
-### Aufgabe 2 ###
-clear;
+for i = [1:length(pZufall)] 
+  EvonN += length(bitList{1,i}) * pZufall(i);
+  Eideal -= pZufall(i)*log2(pZufall(i));
+endfor
 
-xTwo = [ 1.3 2.2 2.9 3.1 4.5 5.7 7.1 8.0 8.7 8.9 9.3 9.9 ];
-yTwo = [ 1.3 1.0 0.85 0.80 0.33 0 -0.4 -0.7 -0.9 -1.0 -1.1 -1.2];
-[a, b] = linearReg(xTwo,yTwo);
-f = @(x) a.*x .+ b;
-xerg = [0:0.1:10];
-subplot(2,3,2);
-plot(xTwo,yTwo, "+", xerg, f(xerg));
-printf("2) Funktion: %d * x + %d \n", a, b);
-
-### Aufgabe 3 ###
-clear;
-
-xThree = [ 1 2 3 3.5 4.5 5.5 6 7 8 8.5 9.5 10 ];
-yThree = [ 11 7.3 4.8 4.1 1.0 0 0.6 2 3.7 4.2 5.6 8 ];
-
-[a,b,c] = quadraticReg(xThree, yThree);
-f = @(x) a .* x .^ 2 .+ b .* x + c;
-xerg = [1:0.1:10];
-subplot(2,3,3);
-plot(xThree,yThree, "+", xerg, f(xerg));
-printf("3) Funktion: %d * x^2 + %d * x + %d \n", a, b, c);
-
-### Aufgabe 4 ###
-clear;
-
-xFour = [ 0.04 0.32 0.51 0.73 1.03 1.42 1.60 ];
-yFour = [ 2.63 1.18 1.16 1.54 2.65 5.41 7.67 ]; 
-
-[a,b,c] = quadraticReg(xFour, yFour);
-f = @(x) a .* x .^ 2 .+ b .* x + c;
-xerg = [0:0.01:2];
-subplot(2,3,4);
-plot(xFour,yFour, "+", xerg, f(xerg));
-printf("4) Funktion: %d * x^2 + %d * x + %d \n", a, b, c);
-
-### Aufgabe 4 ###
-clear;
-
-xFive = [ 1 2 3 4 5 ];
-yFive = [ 1.8395 0.6765 0.2490 0.0915 0.0335 ];
-[a,b] = expReg(xFive, yFive);
-f = @(x) a .* exp(b.*x);
-xerg = [0:0.1:6];
-subplot(2,3,5);
-plot(xFive,yFive, "+", xerg, f(xerg));
-printf("5) Funktion: %d * exp(%d * x) \n", a, b);
+disp("Aufgabe 2)");
+disp(Eideal/EvonN);
